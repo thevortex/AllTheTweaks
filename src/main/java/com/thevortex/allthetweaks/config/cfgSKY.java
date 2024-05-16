efb517e5dc4c4ded6c08a199c7d2b13ada723754
thevortex <james.g.bowley@gmail.com>
2024-05-16 12:19:02 -0600

    updates for atm sky 9

--- src/main/java/com/thevortex/allthetweaks/config/cfgSKY.java
+++ src/main/java/com/thevortex/allthetweaks/config/cfgSKY.java
@@ -5,8 +5,8 @@ import net.minecraft.resources.ResourceLocation;
 
 public class cfgSKY {
 
-	public static long IPCC = 991374348185256036L;
-	public static String ATM = "atm7-sky";
-	public static String DISPLAY = "ATM7: To The Sky";
+	public static long IPCC = 1240726872443060235L;
+	public static String ATM = "atm9-sky";
+	public static String DISPLAY = "ATM9: To The Sky";
 	public static ResourceLocation BACKGROUND = new ResourceLocation(AllTheTweaks.MODID,"textures/gui/title/background.2.png");
 }
