e3b40afbdc76b75e44d3e5209325e79f36ab549b
thevortex <james.g.bowley@gmail.com>
2026-01-14 15:48:21 -0700

    Add conditional checks to proxy methods
    fix crash if mods not present

--- src/main/java/com/thevortex/allthetweaks/proxy/BCCProxy.java
+++ src/main/java/com/thevortex/allthetweaks/proxy/BCCProxy.java
@@ -2,11 +2,17 @@ package com.thevortex.allthetweaks.proxy;
 
 import dev.wuffs.bcc.Config;
 
+import static com.thevortex.allthetweaks.AllTheTweaks.BCC;
+
 public class BCCProxy {
 
 
     public static String getVersion() {
-        return Config.modpackVersion.get();
+        if (BCC) {
+            return Config.modpackVersion.get();
+        } else {
+            return "N/A";
+        }
     }
 }
 
--- src/main/java/com/thevortex/allthetweaks/proxy/MFProxy.java
+++ src/main/java/com/thevortex/allthetweaks/proxy/MFProxy.java
@@ -3,9 +3,13 @@ package com.thevortex.allthetweaks.proxy;
 
 import org.embeddedt.modernfix.core.ModernFixMixinPlugin;
 
+import static com.thevortex.allthetweaks.AllTheTweaks.mfContainer;
+
 public class MFProxy {
     public static boolean brandingEnabled() {
+        if(mfContainer.isEmpty() ){ return false; }
 
-        return ModernFixMixinPlugin.instance.isOptionEnabled("feature.branding.BrandingControlMixin");
+            return ModernFixMixinPlugin.instance.isOptionEnabled("feature.branding.BrandingControlMixin");
+
     }
 }
