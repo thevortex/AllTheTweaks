48b2e3052f3be5d1ae1ba92e08a360fef441c074
thevortex <james.g.bowley@gmail.com>
2026-01-20 11:12:51 -0700

    fixes

--- src/main/java/com/thevortex/allthetweaks/proxy/BCCProxy.java
+++ src/main/java/com/thevortex/allthetweaks/proxy/BCCProxy.java
@@ -2,10 +2,16 @@ package com.thevortex.allthetweaks.proxy;
 
 import dev.wuffs.bcc.Config;
 
+import static com.thevortex.allthetweaks.AllTheTweaks.BCC;
+
 public class BCCProxy {
 
 
     public static String getVersion() {
-        return Config.modpackVersion.get();
+        if (BCC) {
+            return Config.modpackVersion.get();
+        } else {
+            return "N/A";
+        }
     }
 }
--- src/main/java/com/thevortex/allthetweaks/proxy/MFProxy.java
+++ src/main/java/com/thevortex/allthetweaks/proxy/MFProxy.java
@@ -1,11 +1,12 @@
 package com.thevortex.allthetweaks.proxy;
 
-import net.neoforged.fml.ModList;
 import org.embeddedt.modernfix.core.ModernFixMixinPlugin;
+
+import static com.thevortex.allthetweaks.AllTheTweaks.mfContainer;
 
 public class MFProxy {
     public static boolean brandingEnabled() {
+        if(mfContainer.isEmpty() ){ return false; }
 
-        return ModernFixMixinPlugin.instance.isOptionEnabled("feature.branding.BrandingControlMixin");
-    }
+        return ModernFixMixinPlugin.instance.isOptionEnabled("feature.branding.BrandingControlMixin"); }
 }
